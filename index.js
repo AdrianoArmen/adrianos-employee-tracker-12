261c2c88747b7098a6c6f5755c91916a983a3855
AdrianoA <adriano.armendaris@gmail.com>
2021-08-19 04:30:28 -0500

    ascii logo added init function after main logo

--- index.js
+++ index.js
@@ -2,74 +2,84 @@
 const { prompt } = require("inquirer");
 const db = require("./db");
 require("console.table");
+const logo = require("asciiart-logo");
 
-menuPrompt();
+init();
+
+// ASCII character generated logo by dependency
+function init() {
+    const mainLogo = logo({ name: "Employee Tracker" }).render();
+  
+    console.log(mainLogo);
+  
+    displayPrompt();
+  }
 
 
-async function menuPrompt() {
-    const { choice } = await prompt([
-      {
-        type: "list",
-        name: "choice",
-        message: "What would you like to do?",
-        choices: [
-          {
-            name: "View All Employees",
-            value: "VIEW_EMPLOYEES"
-          },
-          {
-            name: "View All Employees By Department",
-            value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
-          },
-          {
-            name: "View All Employees By Manager",
-            value: "VIEW_EMPLOYEES_BY_MANAGER"
-          },
-          {
-            name: "Add Employee",
-            value: "ADD_EMPLOYEE"
-          },
-          {
-            name: "Remove Employee",
-            value: "REMOVE_EMPLOYEE"
-          },
-          {
-            name: "Update Employee Role",
-            value: "UPDATE_EMPLOYEE_ROLE"
-          },
-          {
-            name: "Update Employee Manager",
-            value: "UPDATE_EMPLOYEE_MANAGER"
-          },
-          {
-            name: "View All Roles",
-            value: "VIEW_ROLES"
-          },
-          {
-            name: "Add Role",
-            value: "ADD_ROLE"
-          },
-          {
-            name: "Remove Role",
-            value: "REMOVE_ROLE"
-          },
-          {
-            name: "View All Departments",
-            value: "VIEW_DEPARTMENTS"
-          },
-          {
-            name: "Add Department",
-            value: "ADD_DEPARTMENT"
-          },
-          {
-            name: "Remove Department",
-            value: "REMOVE_DEPARTMENT"
-          },
-          {
-            name: "Quit",
-            value: "QUIT"
-          }
-        ]
-      }
-    ]);
+async function displayPrompt() {
+  const { choice } = await prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        {
+          name: "View All Employees",
+          value: "VIEW_EMPLOYEES",
+        },
+        {
+          name: "View All Employees By Department",
+          value: "VIEW_EMPLOYEES_BY_DEPARTMENT",
+        },
+        {
+          name: "View All Employees By Manager",
+          value: "VIEW_EMPLOYEES_BY_MANAGER",
+        },
+        {
+          name: "Add Employee",
+          value: "ADD_EMPLOYEE",
+        },
+        {
+          name: "Remove Employee",
+          value: "REMOVE_EMPLOYEE",
+        },
+        {
+          name: "Update Employee Role",
+          value: "UPDATE_EMPLOYEE_ROLE",
+        },
+        {
+          name: "Update Employee Manager",
+          value: "UPDATE_EMPLOYEE_MANAGER",
+        },
+        {
+          name: "View All Roles",
+          value: "VIEW_ROLES",
+        },
+        {
+          name: "Add Role",
+          value: "ADD_ROLE",
+        },
+        {
+          name: "Remove Role",
+          value: "REMOVE_ROLE",
+        },
+        {
+          name: "View All Departments",
+          value: "VIEW_DEPARTMENTS",
+        },
+        {
+          name: "Add Department",
+          value: "ADD_DEPARTMENT",
+        },
+        {
+          name: "Remove Department",
+          value: "REMOVE_DEPARTMENT",
+        },
+        {
+          name: "Quit",
+          value: "QUIT",
+        },
+      ],
+    },
+  ]);
 }
