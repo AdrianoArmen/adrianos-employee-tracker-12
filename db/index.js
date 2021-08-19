e3324c303d2d8dca37bb4833caa4509258908ee2
AdrianoA <adriano.armendaris@gmail.com>
2021-08-19 05:12:26 -0500

    switch cases for prompt selection

---
+++ db/index.js
@@ -0,0 +1,7 @@
+const connection = require("./connection");
+
+class DB {
+    // query definition here
+}
+
+module.exports = new DB(connection);
--- index.js
+++ index.js
@@ -8,13 +8,12 @@ init();
 
 // ASCII character generated logo by dependency
 function init() {
-    const mainLogo = logo({ name: "Employee Tracker" }).render();
-  
-    console.log(mainLogo);
-  
-    displayPrompt();
-  }
+  const mainLogo = logo({ name: "Employee Tracker" }).render();
 
+  console.log(mainLogo);
+
+  displayPrompt();
+}
 
 async function displayPrompt() {
   const { choice } = await prompt([
@@ -82,4 +81,49 @@ async function displayPrompt() {
       ],
     },
   ]);
+
+  // switch cases  to trigger functions depending on prompt selection
+  switch (choice) {
+    case "VIEW_EMPLOYEES":
+      return viewEmployees();
+
+    case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+      return viewEmployeesByDepartment();
+
+    case "VIEW_EMPLOYEES_BY_MANAGER":
+      return viewEmployeesByManager();
+
+    case "ADD_EMPLOYEE":
+      return addEmployee();
+
+    case "REMOVE_EMPLOYEE":
+      return removeEmployee();
+
+    case "UPDATE_EMPLOYEE_ROLE":
+      return updateEmployeeRole();
+
+    case "UPDATE_EMPLOYEE_MANAGER":
+      return updateEmployeeManager();
+
+    case "VIEW_DEPARTMENTS":
+      return viewDepartments();
+
+    case "ADD_DEPARTMENT":
+      return addDepartment();
+
+    case "REMOVE_DEPARTMENT":
+      return removeDepartment();
+
+    case "VIEW_ROLES":
+      return viewRoles();
+
+    case "ADD_ROLE":
+      return addRole();
+
+    case "REMOVE_ROLE":
+      return removeRole();
+      
+    default:
+      return quit();
+  }
 }
